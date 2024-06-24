1f8c7ea87596e11a03fbd763c25b7427403290d1
Luciano Belotto <Luciano.Belotto@nxp.com>
2024-06-24 11:19:46 -0400

    2024-06-24 - Book

--- grails-app/domain/org/bookstore/Book.groovy
+++ grails-app/domain/org/bookstore/Book.groovy
@@ -1,6 +1,9 @@
 package org.bookstore
 
 class Book {
+    Date dateCreated;
+    Date lastUpdated;
+
     String title;
 
     static constraints = {
--- grails-app/services/org/bookstore/BookService.groovy
+++ grails-app/services/org/bookstore/BookService.groovy
@@ -1,9 +1,9 @@
 package org.bookstore
 
-import grails.gorm.services.Service
+import grails.gorm.transactions.Transactional
 
-@Service(Book)
-interface BookService {
+
+interface IBookService {
 
     Book get(Serializable id)
 
@@ -14,5 +14,40 @@ interface BookService {
     void delete(Serializable id)
 
     Book save(Book book)
+
+}
+
+@Transactional
+class BookService implements IBookService {
+
+    Book get(Serializable id) {
+        return Book.get(id);
+    }
+
+    Book save(Book book) {
+        book = book.save();
+        return book;
+    }
+
+    List<Book> list(Map args) {
+        Book[] books = [];
+
+        books = Book.list();
+
+        return books;
+    }
+
+    Long count() {
+        Long countOfRecords;
+
+        countOfRecords = Book.count();
+
+        return countOfRecords;
+    }
+
+    void delete(Serializable id) {
+        Book book = this.get(id);
+        book.delete();
+    }
 
 }
